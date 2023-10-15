327d93c0ecdf827871705a51001604c33bc0371f
rafatars <rafael.trigo.azevedo@gmail.com>
2023-10-15 19:45:39 -0300

    Add the vehicle service and its tests

---
+++ src/main/java/com/rafatars/parkingManager/services/impl/VehicleService.java
@@ -0,0 +1,119 @@
+package com.rafatars.parkingManager.services.impl;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import com.rafatars.parkingManager.entities.Vehicle;
+import com.rafatars.parkingManager.entities.VehicleEntity;
+import com.rafatars.parkingManager.entities.Vehicle;
+import com.rafatars.parkingManager.entities.VehicleEntity;
+import com.rafatars.parkingManager.respositories.IVehicleRepository;
+import com.rafatars.parkingManager.services.IVehicleService;
+
+@Service
+public class VehicleService implements IVehicleService {
+
+	private IVehicleRepository vehicleRepository;
+	
+	@Autowired
+	public VehicleService(IVehicleRepository vehicleRepository) {
+		this.vehicleRepository = vehicleRepository;
+	}
+
+	
+	
+	@Override
+	public Vehicle create(Vehicle obj) {
+		
+		final VehicleEntity vehicleEntity = vehicleToVehicleEntity(obj);
+		
+		final VehicleEntity savedVehicle = vehicleRepository.save(vehicleEntity);
+		
+		return vehicleEntityToVehicle(savedVehicle);
+	}
+
+	@Override
+	public Vehicle update(Long id, Vehicle obj) {
+		
+		VehicleEntity vehicle = vehicleRepository.findById(id).get();
+		
+		vehicle.setBrand(obj.getBrand());
+		vehicle.setColor(obj.getColor());
+		vehicle.setModel(obj.getModel());
+		vehicle.setPlate(obj.getPlate());
+		vehicle.setType(obj.getType());
+		vehicle.setCompany(obj.getCompany());
+		
+		final VehicleEntity savedVehicle = vehicleRepository.save(vehicle);
+		
+		return vehicleEntityToVehicle(savedVehicle);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		
+		try {
+			vehicleRepository.deleteById(id);
+
+	    } catch (final EmptyResultDataAccessException ex) {
+	     
+	    }
+		
+	}
+
+	@Override
+	public Optional<Vehicle> findById(Long id) {
+		
+		Optional<VehicleEntity> vehicleEntity = vehicleRepository.findById(id);
+		
+		
+		return vehicleEntity.map(vehicle -> vehicleEntityToVehicle(vehicle));
+	}
+
+	@Override
+	public List<Vehicle> FindAll() {
+		
+		final Iterable<VehicleEntity> vehicleEntities = vehicleRepository.findAll();
+		
+		final List<VehicleEntity> vehiclesEntitiesList = StreamSupport.stream(vehicleEntities.spliterator(), false)
+				.collect(Collectors.toList());
+		
+		return vehiclesEntitiesList.stream().map(vehicle -> vehicleEntityToVehicle(vehicle))
+				.collect(Collectors.toList());
+	}
+	
+	
+	public VehicleEntity vehicleToVehicleEntity(Vehicle vehicle) {
+		return VehicleEntity.builder()
+				.id(vehicle.getId())
+				.brand(vehicle.getBrand())
+				.color(vehicle.getColor())
+				.model(vehicle.getModel())
+				.plate(vehicle.getPlate())
+				.type(vehicle.getType())
+				.company(vehicle.getCompany())
+				.build();
+		
+		
+	}
+	
+	public Vehicle vehicleEntityToVehicle(VehicleEntity vehicle) {
+		return Vehicle.builder()
+				.id(vehicle.getId())
+				.brand(vehicle.getBrand())
+				.color(vehicle.getColor())
+				.model(vehicle.getModel())
+				.plate(vehicle.getPlate())
+				.type(vehicle.getType())
+				.company(vehicle.getCompany())
+				.build();
+	}
+	
+	
+}
---
+++ src/test/java/com/rafatars/parkingManager/services/VehicleServiceTest.java
@@ -0,0 +1,91 @@
+package com.rafatars.parkingManager.services;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.rafatars.parkingManager.TestDataUtil;
+import com.rafatars.parkingManager.entities.Vehicle;
+import com.rafatars.parkingManager.entities.VehicleEntity;
+import com.rafatars.parkingManager.respositories.IVehicleRepository;
+import com.rafatars.parkingManager.services.impl.VehicleService;
+
+@ExtendWith(MockitoExtension.class)
+public class VehicleServiceTest {
+	
+	@Mock
+	private IVehicleRepository vehicleRepository;
+	
+	@InjectMocks
+	private VehicleService underTest;
+	
+	@Test
+	public void testThatVehicleCanBeCreated() {
+		
+		final Vehicle vehicle = TestDataUtil.createTestVehicleWithoutCompanyA();
+		
+		final VehicleEntity vehicleEntity = underTest.vehicleToVehicleEntity(vehicle);
+		
+		when(vehicleRepository.save(vehicleEntity)).thenReturn(vehicleEntity);
+		
+		final Vehicle created = underTest.create(vehicle);
+		
+		assertEquals(created, vehicle);
+		
+	}
+	
+	@Test
+	public void testThatVehicleCanBeUpdated() {
+		final Vehicle oldVehicle = TestDataUtil.createTestVehicleWithoutCompanyA();
+		
+		Vehicle newVehicle = TestDataUtil.createTestVehicleWithoutCompanyB();
+		
+		VehicleEntity savedMockReturn = underTest.vehicleToVehicleEntity(newVehicle);
+		savedMockReturn.setId(oldVehicle.getId());
+		
+		when(vehicleRepository.findById(oldVehicle.getId()))
+			.thenReturn(Optional.of(underTest.vehicleToVehicleEntity(oldVehicle)));
+		
+		when(vehicleRepository.save(savedMockReturn)).thenReturn(savedMockReturn);
+		
+		final Vehicle updatedVehicle = underTest.update(oldVehicle.getId(), newVehicle);
+		
+		newVehicle.setId(oldVehicle.getId());
+		
+		assertEquals(updatedVehicle, newVehicle);
+		
+	}
+	
+	@Test
+	public void testThatTheVehicleCanBeDeleted() {
+		final Long id = 1L;
+		
+		underTest.deleteById(id);
+		
+		verify(vehicleRepository, times(1)).deleteById(id);
+	}
+	
+	@Test
+	public void testThatCanFindVehicleById() {
+		
+		final Vehicle vehicle = TestDataUtil.createTestVehicleWithoutCompanyA();
+		
+		when(vehicleRepository.findById(vehicle.getId()))
+			.thenReturn(Optional.of(underTest.vehicleToVehicleEntity(vehicle)));
+		
+		final Optional<Vehicle> foundVehicle = underTest.findById(vehicle.getId());
+		
+		assertThat(foundVehicle.get()).isEqualTo(vehicle);
+	}
+	
+}
