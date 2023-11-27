1da0b9835b0b8198b4fe1baa04fd363143fb87a1
rafatars <rafael.trigo.azevedo@gmail.com>
2023-11-27 05:57:58 -0300

    Add the ParkingLotEntity and its mirror

---
+++ src/main/java/com/rafatars/parkingManager/entities/ParkingLotEntity.java
@@ -0,0 +1,30 @@
+package com.rafatars.parkingManager.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "parking_lots")
+public class ParkingLotEntity {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    private String name;
+    private String address;
+    private String phone;
+    private int carSpaces;
+    private int motorcycleSpaces;
+    private CompanyEntity company;
+}
