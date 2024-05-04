5cb3a429dd96dd3f02b130040fcd80a572360b51
rafatars <rafael.trigo.azevedo@gmail.com>
2024-05-04 12:19:34 -0300

    Feat: Add findByName method

--- src/main/java/com/rafatars/parkingManager/respositories/IParkingLotRepository.java
+++ src/main/java/com/rafatars/parkingManager/respositories/IParkingLotRepository.java
@@ -1,6 +1,7 @@
 package com.rafatars.parkingManager.respositories;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -10,4 +11,5 @@ import com.rafatars.parkingManager.entities.ParkingLotEntity;
 @Repository
 public interface IParkingLotRepository extends CrudRepository<ParkingLotEntity, Long>{
     List<ParkingLotEntity> findAllByCompanyId(Long companyId);
+    Optional<ParkingLotEntity> findByName(String name);
 }
